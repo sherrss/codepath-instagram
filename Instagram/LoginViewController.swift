3abf3ced7a1743f90eceea6c5f2ac7bc61556bac
sherrss <liu1500sherry@gmail.com>
2022-10-13 13:11:59 -0400

    Add code for sign up and sign in

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -23,10 +23,32 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+          
+        user.signUpInBackground{ (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
 
