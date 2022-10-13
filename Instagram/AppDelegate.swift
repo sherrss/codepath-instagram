cca7ef74a515385834a5c1dd58ca13454362f56c
sherrss <liu1500sherry@gmail.com>
2022-10-13 12:05:42 -0400

    Add app icon and assets

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "yesik9jEIDFaAxb2BjAIXis2RKs7rqp9Dg4peWfz" // <- UPDATE
+                    $0.clientKey = "sCCCLnJyVqMQcjFP0qcc1jblvxNx7whHswHJHQgl" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
