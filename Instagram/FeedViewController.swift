893ac9441d89cab724156c94069a3a6e7cfcae6f
sherrss <liu1500sherry@gmail.com>
2022-10-15 18:29:27 -0400

    Implement add a comment

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -19,6 +19,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var showsCommentBar = false
     
     var posts = [PFObject]()
+    var selectedPost: PFObject!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,6 +69,23 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        let comment = PFObject(className: "Comments")
+        
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()!
+
+        selectedPost.add(comment, forKey: "comments")
+
+        selectedPost.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved!")
+            } else {
+                print("Error saving comment!")
+            }
+        }
+        
+        tableView.reloadData()
         
         
         commentBar.inputTextView.text = nil
@@ -135,21 +153,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             showsCommentBar = true
             becomeFirstResponder()
             commentBar.inputTextView.becomeFirstResponder()
+            
+            selectedPost = post
         }
-        
-//        comment["text"] = "this is a random comment"
-//        comment["post"] = post
-//        comment["author"] = PFUser.current()!
-//
-//        post.add(comment, forKey: "comments")
-//
-//        post.saveInBackground { (success, error) in
-//            if success {
-//                print("Comment saved!")
-//            } else {
-//                print("Error saving comment!")
-//            }
-//        }
     }
     
     /*
